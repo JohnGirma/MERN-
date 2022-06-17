bec400b42f1aacc63f668b5f0410c287341d24e7
JohnGirma <73526402+JohnGirma@users.noreply.github.com>
2022-06-17 12:51:03 +0300

    nodemon

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+const { MongoClient } = require('mongodb')
 const express =require("express")
+let db
 
 const app  =express()
 
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{
+    const allPeoples = await db.collection("people").find().toArray()
+    console.log(allPeoples)
     res.send("welcome")
 })
 app.get("/admin",(req,res)=>{
     res.send("this is the secret")
 })
-app .listen(3000)
+async function start() {
+    // Use connect method to connect to the server
+    const client=new MongoClient("mongodb://root:root@localhost:27017/MearApp?&authSource=admin")
+    await client.connect()
+    db = client.db()
+    console.log('Connected successfully to server')
+    app .listen(3000)
+  
+    // the following code examples can be pasted here...
+  
+  }
+  start()
